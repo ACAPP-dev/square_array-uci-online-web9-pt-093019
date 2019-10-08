9de141a710f3b528fd4b700e469df0ffbb8b0429
Andrew Capp <acapp909@gmail.com>
2019-10-08 23:25:50 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,13 @@
 def square_array(array)
   # your code here
+  new_array=[]
+  array.each do |item|
+    
+    item2 = item * item
+    new_array.push(item2)
+    
+  end
+  
 end
+
+p square_array[1,2,3]
